1a48bb0ba253e2273f4e4be56642fd71a70c7e28
Ronan Vieira <ronanvcjunior@gmail.com>
2021-09-06 15:10:05 -0300

    Project Created

---
+++ backend/src/main/java/com/desuperior/devvendas/DevvendasApplication.java
@@ -0,0 +1,13 @@
+package com.desuperior.devvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desuperior/devvendas/DevvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desuperior.devvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
