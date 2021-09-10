b9bf2e299e207fcf21c75551addc1629f45a7b43
Ronan Vieira <ronanvcjunior@gmail.com>
2021-09-10 14:48:04 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="text-primary">Página Home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
